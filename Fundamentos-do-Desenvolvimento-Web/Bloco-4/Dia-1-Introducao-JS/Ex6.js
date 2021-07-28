ff6604d21d13542c8f5a5bcd854c0d8987f78ef2
Arthur <turmessias@gmail.com>
2021-07-28 19:56:25 -0300

    Exercicios Dia 1 JavaScript

---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-4/Dia-1-Introducao-JS/Ex6.js
@@ -0,0 +1,9 @@
+let rook = "horizontally, vertically";
+
+if (rook = "horizontally") {
+    console.log("move");
+} else if (rook = "vertically") {
+    console.log("move");
+} else {
+    console.log("Dont Move")
+}
---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-4/Dia-1-Introducao-JS/teste2.js
@@ -0,0 +1,13 @@
+let luzSemaforo = "verde";
+
+if (luzSemaforo = "verde") {
+    console.log("Seguir em Frente");
+}
+
+else if (luzSemaforo = "amarelo") {
+    console.log("Atenção!");
+}
+
+else {
+    console.log("Parar Agora");
+}
