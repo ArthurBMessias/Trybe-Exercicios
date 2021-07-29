3386a3743cc88f43f2370853bf2c16866ab085b7
Arthur <turmessias@gmail.com>
2021-07-29 20:50:05 -0300

    Exs do D2 com aux do colega Gabriel

---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-4/Dia-2-Array-loop-For/b4d2e1.js
@@ -0,0 +1,13 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+
+// let searchForLastTask = tasksList [tasksList.length -1 ];
+   //  console.log(searchForLastTask);
+
+// tasksList.push('Jantar');
+    //console.log(tasksList); 
+
+tasksList.unshift('Acordar');
+    console.log(tasksList);
---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-4/Dia-2-Array-loop-For/b4d2e5.js
@@ -0,0 +1,6 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let cont = 0; cont < cars.length; cont +=1) {
+    let meusCarros = 'Eu tenho um ' + cars[cont];
+        console.log (meusCarros);
+}
