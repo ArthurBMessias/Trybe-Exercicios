35d58bfc5cffe7126344b582727477dad84762b4
Arthur <turmessias@gmail.com>
2021-08-02 21:33:02 -0300

    Exercícios Bloco 4 dia 4

---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-4/Dia-4-Objetos-e-Funcoes/B4D4E1.js
@@ -0,0 +1,16 @@
+// baby steps ex 1
+// declarar a função
+// laço for in para percorrer a var personagem
+// console log + mensagem + var
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+//  1 console.log ('Bem-vinda, ' + info['personagem']);
+
+// 2info['recorrente'] = 'Sim'
+ // console.table (info);
+
+ 
