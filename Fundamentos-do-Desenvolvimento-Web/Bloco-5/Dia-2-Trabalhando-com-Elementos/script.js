27f07a6ddb02b98f03f7b45e373770ead66db149
Arthur <turmessias@gmail.com>
2021-08-10 11:23:36 -0300

    Resolve segundo exemplo

---
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-5/Dia-2-Trabalhando-com-Elementos/script.js
@@ -0,0 +1,6 @@
+function brotherFather () {
+    let father = document.querySelector('#pai');
+    let createUncle = document.createElement('section');
+    father.appendChild(createUncle);
+}
+brotherFather()
