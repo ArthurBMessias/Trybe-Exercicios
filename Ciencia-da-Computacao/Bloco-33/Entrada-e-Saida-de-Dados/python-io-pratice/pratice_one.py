25b6d5ef5365b7a771256f72c9a0bd99a57cffb0
Arthur Bernardo Messias <turmessias@gmail.com>
2022-06-29 17:54:58 -0300

    Exercicios do Dia

---
+++ Ciencia-da-Computacao/Bloco-33/Entrada-e-Saida-de-Dados/python-io-pratice/pratice_one.py
@@ -0,0 +1,4 @@
+nome_usuario = input("Digite seu nome: ")
+
+for letter in nome_usuario:
+    print(letter)
---
+++ Ciencia-da-Computacao/Bloco-33/Entrada-e-Saida-de-Dados/python-io-pratice/sum_num.py
@@ -0,0 +1,10 @@
+# meu_numero = input("digite seu número:")
+
+# print(meu_numero)
+
+meu_numero = 0
+
+while meu_numero < 42:
+    meu_numero += int(input("Digite seu número: "))
+
+print("A soma passou de 42")
